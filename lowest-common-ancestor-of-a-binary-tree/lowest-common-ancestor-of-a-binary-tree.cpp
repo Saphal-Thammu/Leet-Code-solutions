4996546816523c0f826f23609dc5d2ec256bd5cc
Saphal-Thammu <76019537+Saphal-Thammu@users.noreply.github.com>
2021-02-14 18:59:29 -0700

    Time: 20 ms (47.34%), Space: 14.1 MB (84.74%) - LeetHub

--- lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,4 +1,66 @@
-// 
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+//         vector<TreeNode*> path1;
+//         vector<TreeNode*> path2;
+        
+//         backtrack(root,p,path1);
+//         backtrack(root,q,path2);
+        
+// //         for(auto it:path1)
+// //         cout<<it;
+        
+// //         cout<<endl;
+// //         // cout<<2;
+        
+// //         for(auto it:path2)
+// //         cout<<it;
+        
+//         for(int i=0;i<path1.size();i++){
+//             if(path1.at(i) != path2.at(i)){
+//                 return path1.at(i-1);
+//             }
+//         }
+//         return NULL; 
+        
+//     }
+    
+// private:
+//     void backtrack(TreeNode* root, TreeNode* p,vector<TreeNode*> path){
+//         //base
+//         if(root==NULL) return; // when we reach the end
+        
+//         if(root==p){
+//             path.push_back(root); // this is to remove the overhead of checking the size while finding the LCA because we have two same nodes we will surely get false when check each node of path1 and path2
+//         }
+        
+//         //logic
+//         //action
+//         path.push_back(root);
+        
+//         //recursion
+//         backtrack(root->left,p,path);
+//         if(path.at(path.size()-1) ==  p) return;
+        
+//         backtrack(root->right,p,path);
+//         if(path.at(path.size()-1) ==  p) return;
+        
+//         //backtrack
+        
+//         path.resize(path.size()-1);
+//     }
+    
+// }; 
 
 // We are using two variables one left node and the other as right node.
 class Solution {
