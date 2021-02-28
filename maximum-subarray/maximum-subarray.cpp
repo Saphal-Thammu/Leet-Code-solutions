3cb63a20e8923ef8e8cffdb07b2fdc726b5575d8
Saphal-Thammu <76019537+Saphal-Thammu@users.noreply.github.com>
2021-02-28 10:41:40 -0700

    Time: 8 ms (73.28%), Space: 13.1 MB (70.70%) - LeetHub

--- maximum-subarray/maximum-subarray.cpp
+++ maximum-subarray/maximum-subarray.cpp
@@ -1,18 +1,63 @@
+// //When we just have to find maximum sum and not the starting and ending indices
+
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+        
+//         if(nums.empty()) return 0;
+        
+//         int n=nums.size();
+//         int gmax=0;
+//         int rSum=0;
+        
+//         for(int i=0;i<n;i++){
+//             rSum=max(rSum+nums[i],nums[i]);
+//             gmax=max(rSum,gmax);
+//         }
+        
+//         return gmax;
+//     }
+// };
+
+
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int maxHere = 0;
-        int maxSoFar = nums[0];
-        
-        if(nums.size() == 1)
-            return nums[0];
-        
-        for(int i = 0;i<nums.size();i++)
-        {
-            maxHere += nums[i];
-            maxSoFar = max(maxSoFar, maxHere);
-            maxHere = max(maxHere, 0);
-        }
-        return maxSoFar;
-    }
+    int maxSubArray(vector<int>& nums) {
+        
+        if(nums.empty()) return 0;
+        
+        int n=nums.size();
+        int gmax=nums[0]; //finding the maximum subarray
+        int rSum=0; //finding the runnng sum
+        
+        //starting and ending indices of global maximum array
+        int start=0; //should change when max value changes
+        int end=0;
+        
+        //to update global maximum start and end we must know curr start and end.
+        //for curr start we will maintain pointer but the end will be i which is in the below for loop
+        int curr=0; //should change when incoming element is greater than rSum
+        
+        for(int i=0;i<n;i++){
+            
+            rSum=rSum+nums[i];
+            
+            if(nums[i]>=rSum){
+                rSum=nums[i];
+                int curr=i;
+            }
+            
+            if(gmax < rSum){
+                gmax=rSum;
+                start=curr;
+                end=i;
+            }
+            
+        }
+        
+        // cout<<start<<endl;
+        // cout<<end<<endl;
+        
+        return gmax;
+    }
 };
