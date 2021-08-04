909af913d3b3f3ebefb695aebec813eba89d8974
Saphal-Thammu <76019537+Saphal-Thammu@users.noreply.github.com>
2021-08-04 15:35:55 -0600

    Time: 332 ms (10.68%), Space: 21.7 MB (35.35%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.py
+++ product-of-array-except-self/product-of-array-except-self.py
@@ -1,35 +1,27 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
-        if len(nums) == 0:
-            return []
+        answer=[0]*len(nums)
+        l=[0]*len(nums)
+        r=[0]*len(nums)
         
-        l1=1;
-        r1=1;
+        l[0]=1
+        r[len(nums)-1]=1
+        lprod=1
+        rprod=1
         
-        result=[0]*len(nums)
-        lm=[0] * len(nums)
-        rm=[0] * len(nums)
         
-        lprod=1;
-        rprod=1;
-        
-        for i in range(len(nums)):
-            lprod=l1*lprod
-            lm[i]=lprod
-            l1=nums[i]
+        for i in range(1,len(nums)):
+            lprod=nums[i-1]*lprod
+            l[i]=lprod
+            print(l[i])
             
-        for i in range(len(nums)-1,-1,-1):
-            rprod=r1*rprod
-            rm[i]=rprod
-            r1=nums[i]
-        
-                    
-        for i in range(len(nums)):
-            result[i]=lm[i]*rm[i]
-
-        
-        return result
-    
-    
-        
+        for i in range(len(nums)-2,-1,-1):
+            rprod=nums[i+1]*rprod
+            r[i]=rprod
+            print (r[i])
+            
+        for i in range(0,len(nums)):
+            answer[i]=l[i]*r[i]
+            
+        return answer
