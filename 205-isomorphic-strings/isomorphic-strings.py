cbb6b068db1c4e3c166af72232fdccc4de99c577
Saphal-Thammu <76019537+Saphal-Thammu@users.noreply.github.com>
2023-12-12 02:03:37 -0700

    Time: 47 ms (53.67%) | Memory: 16.6 MB (35.66%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,40 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        
+        if s is None or t is None:
+            return False
+
+        if len(s) != len(t):
+            return False
+
+        my_dict = dict()
+        my_set = set()
+
+        for i in range(len(s)):
+
+            if s[i] not in my_dict:
+                my_dict[s[i]] = t[i]
+                if t[i] in my_set:
+                    return False
+                my_set.add(t[i])
+
+            if s[i] in my_dict:
+                if my_dict[s[i]] != t[i]:
+                    return False
+
+        return True
+
+        # for i in range(len(s)):
+            
+        #     if s[i] in my_dict:
+        #         if t[i] == my_dict[s[i]] and t[i] not in my_set:
+        #             continue
+        #         else:
+        #             return False
+            
+        #     if s[i] not in my_dict:
+        #         my_dict[s[i]] = t[i]
+        #         my_set.add(t[i])
+
+
+        # return True
