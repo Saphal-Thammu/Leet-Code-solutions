9f0e0085ca2638ab9e9cdc9a7f471ab852fdb3ca
Saphal-Thammu <76019537+Saphal-Thammu@users.noreply.github.com>
2023-12-19 20:28:24 -0700

    Time: 946 ms (41.51%) | Memory: 36.2 MB (22.38%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        
+        map_d = {0 : -1}
+
+        tsum = 0
+
+        for i,val in enumerate(nums):
+            
+            tsum = tsum + val
+            reminder = tsum % k
+
+            # print(reminder)
+            # print("i is {}".format(i))
+
+            # if reminder not in map_d:
+            #     map_d[reminder] = i
+            
+            # print("map_d[reminder] is {}".format(map_d[reminder]))
+
+            if reminder in map_d:
+                diff = i - map_d[reminder]
+                print("diff is {}".format(diff))
+                if ( diff ) >= 2:
+                    return True
+            else:
+                map_d[reminder] = i
+
+        return False
