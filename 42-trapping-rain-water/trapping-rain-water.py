f263e9344d5b7f0e0956eb436744947474dd68ca
Saphal-Thammu <76019537+Saphal-Thammu@users.noreply.github.com>
2024-01-03 19:20:22 -0700

    Time: 99 ms (93.74%) | Memory: 19.4 MB (5.93%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,55 +1,31 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         
+        # This is space complexity of 0(1) solution
+        # The logic is figuring out the [ min(max_height_from_left, max_height_from_right) - height at that point ] 
+        # Left pointer starts at 0th index and right at the end of the height list
+        # We will have two variable max_left_height and max_right_height
+        # Their values will be the values at left pointer and right pointer, however initially we will have zero as we need two walls for water
 
-        # the logic is figuring out the [ min(max_height_from_left, max_height_from_right) - height at that point ] 
+        left, right = 0, len(height) - 1
 
-        # We write a list of max left height at every indice by going through the height list once
-        # Again going and figuring out the max right height at every index by going throught the heightlist again once more.
-        # Now again i go one more time through both the lists and writing another list of min(max_height_from_left, max_height_from_right) 
-        # Now we need to pass through the height list again and do this formula: 
-        # [ min(max_height_from_left, max_height_from_right) - height at that point ]
-        # if negative we do not calculate that position and mark as zero
-        # if positive we get that position the amount of water to be stored
-        # summing the array now will get us total water to be stored.
+        max_left_height = height[left]
+        max_right_height = height[right]
 
+        result = 0
 
-        max_left_height_list = [None] * len(height) 
-        max_right_height_list = [None] * len(height) 
-        min_height_at_index = [None] * len(height) 
-        result_list = [None] * len(height)
-
-        temp_height = height[0]
-
-        for i in range(len(height)):
-
-            temp_height = max(temp_height, height[i])
-            max_left_height_list[i] = temp_height
-
-
-        temp_height = height[len(height) - 1]
-
-        for i in range(len(height)-1, -1, -1):
-
-            temp_height = max(temp_height, height[i])
-            max_right_height_list[i] = temp_height
-
-
-        temp_height = height[0]
-
-        for i in range(len(height)):
-
-            temp_height = min(max_left_height_list[i], max_right_height_list[i])
-            min_height_at_index[i] = temp_height
-
-
-        for i in range(len(height)):
-
-            diff = min_height_at_index[i] - height[i]
+        while left < right:
             
-            if diff <= 0:
-                result_list[i] = 0
-            else:
-                result_list[i] = diff
+            if max_left_height < max_right_height:
+                left = left + 1
+                max_left_height = max(max_left_height, height[left])
+                result = result + (max_left_height - height[left])
 
-        return sum(result_list)
+            else:
+                right = right - 1
+                max_right_height = max(max_right_height, height[right])
+                result = result + (max_right_height - height[right])
+
+        return result
+
+            
